6032f6ecdf6eff94f70536197ec89fb224ee2104
Vincent <vintzs98@yahoo.com>
2022-07-21 01:16:49 +0800

    Small fix
    Keep alive and Readme fix

--- src/mongo-conn.js
+++ src/mongo-conn.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const ConnectDB = async() => {
     await mongoose.connect(
         `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER}/?retryWrites=true&w=majority`, {
-            // keepAlive: true,
+            keepAlive: true,
             dbName: "SerBot"
         }
     );
